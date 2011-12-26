b335df3520a0caddd983ee96abd184d41d41a9e6
benny.saxen <benny.saxen@315517bd-f386-4b40-d418-10b424697b83>
2011-12-26 13:00:13 +0000

    user handling

--- pv.php
+++ pv.php
@@ -6,6 +6,7 @@ function pv()
 {
   global $par;
 
+  $user = $par['user'];
   $func = $par['pv'];
   $inc  = $par['inc'];
   $inc++;
@@ -13,14 +14,9 @@ function pv()
 
   if($inc==1)viking_7_script(1);
   if($inc==3)viking_7_mainmenu(1);
-
   if($inc==4)viking_4_login_logout();
-
   if($inc==2)viking_4_login_Form();
-  if($inc==11)viking_4_adduser_Form();
-
   if($inc==10)viking_4_showUserLoggedIn();
-  if($inc==14)viking_4_addUser_Link();
   if($inc==11)viking_7_current(1);
 
   //==================================  
@@ -28,11 +24,11 @@ function pv()
     {
       //if($inc==9)viking_7_library(1);
     }
-  if($func == 'load')
+  if($func == 'load' && $user && $user != 'guest')
     {
       if($inc==6)viking_7_load(1);
     }
-  if($func == 'board')
+  if($func == 'board' && $user && $user != 'guest')
     {
       if($inc==5)viking_7_menu(1);
       //if($inc==9)viking_7_winLog(1);
@@ -41,14 +37,14 @@ function pv()
       //if($inc==7)viking_7_isMap(1);
       if($inc==6)viking_7_pinValues(1);
     }
-  if($func == 'sketch')
+  if($func == 'sketch' && $user && $user != 'guest')
     {
       if($inc==5)viking_7_menu(1);
       if($inc==6)viking_7_data(1);
       if($inc==7)viking_7_anyFile(1);
       if($inc==9)viking_7_winSerLog(1);
     }
-  if($func == 'log')
+  if($func == 'log' && $user && $user != 'guest')
     {
       if($inc==5)viking_7_menu(1);
       if($inc==6)viking_7_winSerial(1);
@@ -56,11 +52,20 @@ function pv()
     }
   if($func == 'help')
     {
-      if($inc==3)viking_4_addUser_Link();
+      //if($inc==3)viking_4_addUser_Link();
     }
   if($func == 'register')
     {
       if($inc==6)viking_7_applyAccount(1);
+    }
+  if($func == 'admin' && $user == 'admin')
+    {
+      if($inc==6)viking_4_showUserByName();
+
+      if($inc==9) viking_4_addUser_Link();
+      if($inc==9) viking_4_delUser_Link();
+      if($inc==9) viking_4_delUser_Form();
+      if($inc==9)viking_4_adduser_Form();
     }
   if($func == 'pv')
     {
